1f955f6f5fbf78f0b8833d71196166c95df55044
antonio <anto.mirandaa@duocuc.cl>
2025-10-23 14:02:02 +0000

    dios de la zaza

---
+++ app/blogs/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <h1>BLogs</h1>
+      <h1>BLogs#1</h1>
+      <body>Ahora tenemos envios a todo chile</body>
+      <h1>BLogs#2</h1>
+      <body>
+        Nuvea colaboracion con [marca], aprovecha la preventa antes de que te
+        quedes sin los peluches de [marca]
+      </body>
+    </>
+  );
+}
