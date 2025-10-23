582dbb66d4418485dc660f90b4dbbe5bc52ad850
antonio <anto.mirandaa@duocuc.cl>
2025-10-23 13:33:45 +0000

    asd

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
+//import Image from "next/image";
 
 export default function Home() {
   return <h1>Productos</h1>;
 
-  function MiImagen() {
+  /*function MiImagen() {
     return (
       <Image
-        src="https://rimage.ripley.cl/home.ripley/Attachment/MKP/9567/MPM10001713603/full_image-1"
+        src="/images/images.jpg"
         alt="iamgen panda"
         width={50}
         height={30} //dasdad
       />
     );
-  }
+  }*/
 }
