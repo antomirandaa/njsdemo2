b931e730941b69206500942abf7d4f9802e8acc5
antonio <anto.mirandaa@duocuc.cl>
2025-10-23 13:15:57 +0000

    adssad

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return <h1>Productos</h1>;
 
-  function MiImagen() {
+  /* function MiImagen() {
     return (
       <Image
         src="https://rimage.ripley.cl/home.ripley/Attachment/MKP/9567/MPM10001713603/full_image-1"
@@ -12,5 +12,5 @@ export default function Home() {
         height={30} //dasdad
       />
     );
-  }
+  }*/
 }
