5da1666f36ad4e141d603e0c1ae20c04345f78d5
antonio <anto.mirandaa@duocuc.cl>
2025-11-01 20:59:53 +0000

    asdas

--- data/ofertas.jsx
+++ data/ofertas.jsx
@@ -1,1 +1,29 @@
 //productos en oferta
+export const ofertas = [
+  {
+    nombre: "Snoopy",
+    precio: 10000,
+    imagen: "images/peluches/Snoopy/Snoopy1.PNG",
+  },
+  {
+    nombre: "Labubu",
+    precio: 12000,
+    imagen: "images/ofertas/IMG_8783.jpg",
+  },
+  {
+    nombre: "Oso Corazon",
+    precio: 14000,
+    imagen: "images/ofertas/D_NQ_NP_882626-MLA48522604665_122021-O.webp",
+  },
+  {
+    nombre: "Oso con Flotador",
+    precio: 16000,
+    imagen:
+      "images/ofertas/desktop-wallpaper-kawaii-plushies-cute-stuffed-animals-pinterest-plushies.jpg",
+  },
+  {
+    nombre: "Osa",
+    precio: 14000,
+    imagen: "images/ofertas/54f7580b4b1f886170d4eea9847f5f8d.jpg",
+  },
+];
